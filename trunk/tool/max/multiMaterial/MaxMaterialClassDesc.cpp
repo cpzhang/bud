17f73542d80a6900e1bdf0045d2f03ff979678b3
cp <297191409@qq.com>
2012-07-22 17:07:59 +0800

    remove max

--- trunk/tool/max/multiMaterial/MaxMaterialClassDesc.cpp
+++
@@ -1,7 +0,0 @@
-#include "MaxMaterialClassDesc.h"
-
-ClassDesc2* GetMaxMaterialDesc() 
-{ 
-	static MaxMaterialClassDesc MaxMaterialDesc;
-	return &MaxMaterialDesc; 
-}
--- trunk/tool/max2012/preview/Include/stdafx.h
+++
@@ -1,7 +0,0 @@
-#define _WIN32_WINDOWS	0x0501
-
-#include "windows.h"
-#include "Base.h"
-#pragma comment(lib, MAKE_DLL_LIB_NAME(Base))
-#include "re.h"
-using namespace rkt;
